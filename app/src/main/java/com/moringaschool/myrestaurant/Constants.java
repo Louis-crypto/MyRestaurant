91260a5511df1fee4f86329b4ee7e85ccb0ce93a
Louis-crypto <kinglouis745@gmail.com>
2020-03-04 12:40:00 +0300

    alot f things changed

---
+++ app/src/main/java/com/moringaschool/myrestaurant/Constants.java
@@ -0,0 +1,8 @@
+package com.moringaschool.myrestaurant;
+
+import com.moringaschool.myrestaurant.BuildConfig;
+
+public class Constants {
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/";
+    public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
+}
--- app/src/test/java/com/moringaschool/myrestaurant/MainActivityTest.java
+++ app/src/test/java/com/moringaschool/myrestaurant/MainActivityTest.java
@@ -1,4 +1,30 @@
 package com.moringaschool.myrestaurant;
 
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.TestCase.assertTrue;
+
+@Config(sdk = Build.VERSION_CODES.O_MR1)
+@RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup(){
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent(){
+        TextView appNameTextView = activity.findViewById(R.id.appNameTextView);
+        assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
+    }
 }
